e65191eca4e0b21c6d800dce7ad253c7dcc7a005
Hammad Zaeem <70287776+KHa321Z@users.noreply.github.com>
2024-01-07 18:58:04 +0500

    Create 23L-0700_Q1.cpp

---
+++ Lab/Assignment 3/23L-0700_Q1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	cout << "This is the output\n";
+	cout << "This.\t Is.\t The.\t Output.\n";
+	cout << "This. \nIs. \nthe \noutput\n";
+
+	return 0;
+
+};
